e2fd838bc8b17d9705c24b4ad8667c1ac98f7b65
pawelmisala <paweljozefmisala@gmail.com>
2025-10-08 13:32:22 +0200

    Finished cpp01

--- cpp01/cpp01.cpp
+++ cpp01/cpp01.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//function for exercise 4
 void minMaxRep(int a[], size_t size, int& mn, size_t& in, int& mx, size_t& ix){
 	if (size == 0) {
 		mn = mx = 0;
@@ -29,6 +30,45 @@ void minMaxRep(int a[], size_t size, int& mn, size_t& in, int& mx, size_t& ix){
 		}
 	}
 }
+
+//functions for exercise 5
+void ord3(double& a, double& b, double& c) {
+	if (a > b) swap(a, b);
+	if (a > c) swap(a, c);
+	if (b > c) swap(b, c);
+}
+void ord3(double* a, double* b, double* c) {
+	if (*a > *b) swap(*a, *b);
+	if (*a > *c) swap(*a, *c);
+	if (*b > *c) swap(*b, *c);
+}
+void getMinMax(const double &a, const double& b, const double& c, const double*& pMin, const double*& pMax) {
+    if (a <= b && a <= c) pMin = &a;
+	else if (b <= a && b <= c) pMin = &b;
+	else pMin = &c;
+
+	if (a >= b && a >= c) pMax = &a;
+	else if (b >= a && b >= c) pMax = &b;
+	else pMax = &c;
+}
+void getMinMax(const double *a, const double* b, const double* c, const double** pMin, const double** pMax) {
+    if (*a <= *b && *a <= *c) *pMin = a;
+	else if (*b <= *a && *b <= *c) *pMin = b;
+	else *pMin = c;
+
+	if (*a >= *b && *a >= *c) *pMax = a;
+	else if (*b >= *a && *b >= *c) *pMax = b;
+	else *pMax = c;
+}
+
+void printOrd(const double* a, const double* b, const double* c) {
+    cout << *a << " " << *b << " " << *c << endl;
+}
+void printMinMax(const double* pmn, const double* pmx) {
+    cout << "Min = " << *pmn << "; " << "Max = " << *pmx << std::endl;
+}
+
+
 void zadanie1() {
 	int input = -1;
 	int numberWithHighestSum = 0;
@@ -115,14 +155,39 @@ void zadanie4(){
 	cout << "Max: " << mx << " " << ix << " times" << endl;
 }
 void zadanie5(){
-	// not implemented
+	double a, b, c;
+    const double *pMin, *pMax;
+
+    a = 2; b = 1; c = 3;
+    ord3(a,b,c);
+    printOrd(&a, &b, &c);
+
+    a = 3; b = 2; c = 1;
+    ord3(&a,&b,&c);
+    printOrd(&a, &b, &c);
+
+    a = -1; b = -1; c = 1;
+    ord3(&a,&b,&c);
+    printOrd(&a, &b, &c);
+
+    a = 2; b = 3; c = 1;
+    getMinMax(a,b,c,pMin,pMax);
+    printMinMax(pMin, pMax);
+
+    a = 3; b = 1; c = 2;
+    getMinMax(&a,&b,&c,&pMin,&pMax);
+    printMinMax(pMin, pMax);
+
+    a = 3; b = 3; c = -1;
+    getMinMax(&a,&b,&c,&pMin,&pMax);
+    printMinMax(pMin, pMax);
 }
 
 int main() {
 	int enteredNumber = -1;
 
 	while (true){
-		cout << "enter exercise number (1-4) (0 to exit): ";
+		cout << "enter exercise number (1-5) (0 to exit): ";
 		cin >> enteredNumber;
 
 		if (enteredNumber == 0) break;
